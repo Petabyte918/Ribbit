d555e6664f34c61531979d39d290a52319bba57b
Kevin <Kevin.Kuan@stonybrook.edu>
2017-03-27 19:19:48 -0400

    BENCHMARK 1 3/27/2017

--- js/PathfinderMain.js
+++
@@ -1,8 +0,0 @@
-var top_down = top_down || {};
-top_down.game = new Phaser.Game(768, 512, Phaser.AUTO, 'phaser-game');
-
-top_down.game.state.add('Boot', top_down.Boot);
-top_down.game.state.add('Preload', top_down.Preload);
-top_down.game.state.add('Game', top_down.Game);
-
-top_down.game.state.start('Boot');
